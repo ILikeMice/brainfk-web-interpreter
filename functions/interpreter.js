b024db7e380c40be5d31cf02636cdd555555147c
ILikeMice <ilikemice@posteo.de>
2024-12-19 22:03:39 +0000

    perchance?

--- functions/interpreter.js
+++ functions/interpreter.js
@@ -5,18 +5,19 @@ const app = express();
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    let code = req.query.code.trim()
-    let input = req.query.input
+    let code = req.query.code.trim();
+    let input = req.query.input;
 
     class Token {
-        constructor(type) {
+        constructor(type, value = null) {
             this.type = type;
+            this.value = value;
         }
     }
-    
+
     class Interpreter {
         constructor(text, inp) {
-            this.input = inp
+            this.input = inp;
             this.text = text;
             this.pos = 0;
             this.current_token = null;
@@ -27,33 +28,33 @@ router.get("/", async (req, res) => {
                 0: 0,
             };
         }
-    
+
         makeError(num) {
             console.error("Error! code: " + num);
         }
-    
+
         next_token() {
             let text = this.text;
-    
+
             if (this.pos > text.length - 1) {
                 return new Token("EOF");
             }
-    
+
             let current_char = text[this.pos];
-            console.log(current_char)
             if (["+", "-", ">", "<", "[", "]", ",", "."].includes(current_char)) {
                 this.pos++;
                 return new Token(current_char, current_char);
             } else {
-                this.makeError(1);
+                this.pos++;
+                return this.next_token(); 
             }
         }
-    
+
         find_loops() {
             let used = [];
             let pointer = 0;
             let code = this.text;
-    
+
             for (let i = 0; i < code.length; i++) {
                 let startends = 0;
                 if (code[i] == ">") {
@@ -71,85 +72,84 @@ router.get("/", async (req, res) => {
                             if (code[b] == "]") {
                                 startends--;
                             }
-    
+
                             if (startends == 0) {
                                 this.loops[b] = [i, pointer];
                                 used.push(i, b);
                                 break;
                             }
-                        }   
+                        }
                     }
                 }
             }
         }
-    
+
         expr() {
             this.find_loops();
-            console.log(this.loops);
             let output = "";
-            let usrinput = this.input; // add the input from get req here
+            let usrinput = this.input.split(''); 
             this.current_token = this.next_token();
-            console.log(this.current_token)
-            while (this.current_token.type != "EOF") {
+            while (this.current_token && this.current_token.type != "EOF") {
                 let currtokentype = this.current_token.type;
                 if (currtokentype == "]") {
                     if (
                         this.loops.hasOwnProperty(this.pos - 1) &&
                         this.memory[this.loops[this.pos - 1][1]] != 0
                     ) {
-                        console.log(
-                            "looping",
-                            this.memory[this.loops[this.pos - 1][1]]
-                        );
                         this.pos = this.loops[this.pos - 1][0];
                     }
                 }
-    
+
                 if (currtokentype == "+") {
                     this.memory[this.current_pointer]++;
                 }
-    
+
                 if (currtokentype == "-") {
                     this.memory[this.current_pointer]--;
                 }
-    
+
                 if (currtokentype == ".") {
                     output += String.fromCharCode(
                         this.memory[this.current_pointer]
                     );
                 }
-    
+
                 if (currtokentype == ",") {
-                    this.memory[this.current_pointer] = String.fromCodePoint(
-                        usrinput[0]
-                    );
-                    usrinput[0] = "";
+                    if (usrinput.length > 0) {
+                        this.memory[this.current_pointer] = usrinput.shift().charCodeAt(0);
+                    } else {
+                        this.memory[this.current_pointer] = 0;
+                    }
                 }
-    
+
                 if (currtokentype == ">") {
                     this.current_pointer++;
                     if (!this.memory.hasOwnProperty(this.current_pointer)) {
                         this.memory[this.current_pointer] = 0;
                     }
                 }
-    
+
                 if (currtokentype == "<") {
                     this.current_pointer--;
                     if (!this.memory.hasOwnProperty(this.current_pointer)) {
                         this.memory[this.current_pointer] = 0;
                     }
                 }
-    
+
                 this.current_token = this.next_token();
             }
-            console.log(this.memory);
             return output;
         }
     }
 
-   return new Interpreter(code, input).expr()
-})
-
+    try {
+        const result = new Interpreter(code, input).expr();
+        res.send(result);
+    } catch (error) {
+        console.error("Error during interpretation:", error);
+        res.status(500).send("An error occurred during code interpretation.");
+    }
+});
 
 app.use("/.netlify/functions/interpreter", router);
 module.exports.handler = serverless(app);
